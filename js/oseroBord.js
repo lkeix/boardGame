02526533dc275f81ec64d48031dbcd60a9c279de
KeitaMuto <19238462@edu.cc.saga-u.ac.jp>
2020-11-16 18:18:48 +0900

    create oselo data structure script

---
+++ js/oseroBord.js
@@ -0,0 +1,30 @@
+var board = []
+
+function NewBoard() {
+    // [i][j]の位置にnextとcolorのデータを入れる.
+    for (let i = 0; i < 8; i++) {
+        let tmpA = []
+        for (let j = 0; j < 8; j++) {
+            // nextは次の手の位置, colorはコマの色
+            if ((i == 3 || i == 4) && i == j) {
+                tmpA.push({
+                    next: null.
+                    color: "white"
+                })
+            }
+            else if ((i == 3 && j == 4) || (j == 3 && i == 4)) {
+                tmpA.push({
+                    next: null.
+                    color: "black"
+                })
+            }
+            else {
+                tmpA.push({
+                    next: null.
+                    color: ""
+                })
+            }
+        }
+        board.push(tmpA)
+    }
+}
